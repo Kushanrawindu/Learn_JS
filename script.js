ac9ee13fc84d91edfd076780bf9e3ac702d137c9
Kushanrawindu <kushanrawindu@gmail.com>
2022-12-06 00:04:47 +0530

    learn variables

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+console.log("test");
+
+//VARIABLES
+var variableName = 'This is a variable 1';
+console.log(variableName);
+
+let variableName2 = 'This is a variable 2';
+console.log(variableName2);
+
+const variableName3 = 'This is a variable 3';
+console.log(variableName3);
+
+
+
+var variableName = 'This is a variable 1';           //we can't re-assign values for const variables
+console.log(variableName);
+
+variableName = 'Hello variables';
+console.log(variableName);
